0d8266df6d98950fafc95bf837bde6ec626be08e
Miststorm <bishop.jared.michael@gmail.com>
2023-08-17 10:56:17 -0600

    Catching errors assignment - not complete

---
+++ CatchingErrors.java
@@ -0,0 +1,33 @@
+public class CatchingErrors {
+    //Some test code
+    public static void main (String[] args) {
+        int speed = 60;
+        System.out.println(40 <= speed && speed <=70);
+    }
+}
+
+/*
+1.  (age < 6) || (age > 60)
+
+2. (grade == 'A') || (grade == 'B')
+
+
+3. (40 <= speed) && (speed <=70)
+
+
+4. (speed >= 40) && (speed <= 70)
+
+
+5. (age > 12) && (age < 20)
+
+6. (credits >= 90) && (grade == 'A' || 'B')
+
+
+7. (credits < 30) || (credits < 60 && 2.0 > gpa)
+
+
+
+
+
+Next Section:
+ */
