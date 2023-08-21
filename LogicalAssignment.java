a647ffc82ead6fa4142b24e70db26d2cd5de018e
Jared <bishop.jared.michael@gmail.com>
2023-08-21 10:03:08 -0600

    logic assignment -_-

---
+++ LogicalAssignment.java
@@ -0,0 +1,91 @@
+public class LogicalAssignment {
+    
+}
+/*Which of these sentences are propositions? What are the truth values of those that are propositions?
+Boston is the capital of Massachusetts.  Proposition: True.
+
+Miami is the capital of Florida. Proposition: False.
+
+2 + 3 = 5. Proposition: True.
+
+5 + 7 = 10. Proposition: False.
+
+x + 2 = 11. Not a Proposition
+
+Answer this question. Not a proposition.
+
+
+
+What is the negation of each of these propositions?
+Mei does not have an MP3 player.
+
+There is pollution in New Jersey.
+
+2 + 1 !=3
+
+ The summer in Maine is not hot and sunny.
+
+
+
+Let p and q be the propositions “Swimming at the New Jersey shore is allowed” and “Sharks have been spotted near the shore,” respectively. Express each of these compound propositions as an English sentence.
+Sharks have not been spotted near the shore.
+
+Swimming at the New Jersey shore is allowed, and sharks have been spotted near the shore.
+
+Swimming at the New Jersey shore is not allowed, or sharks have been spotted near the shore.
+
+ If swimming at the New Jersey shore is allowed, then sharks have not been spotted near the shore.
+
+If sharks have not been spotted near the shore, then swimming at the New Jersey shore is allowed.
+
+If swimming at the New Jersey shore is not allowed, then sharks have not been spotted near the shore.
+
+Swimming at the New Jersey shore is allowed, if, and only if, sharks have not been spotted near the shore.
+
+ Swimming at the New Jersey shore is not allowed, unless it's been stated that it is allowed, or sharks have not been spotted near the shore.
+
+Let p and q be the propositions
+
+p: It is below freezing.
+
+q: It is snowing.
+
+Write these propositions using p and q and logical connectives (including negations).
+
+
+(p ∧ q)
+
+(p ∧  ¬q)
+
+ (¬p ∧  ¬q) 
+
+ (p ∨ q)
+
+ (p → q)
+
+ (p∨q) ∧(q→¬p)
+
+ (p↔q)
+
+Let p, q, and r be the propositions
+
+p: Grizzly bears have been seen in the area.
+
+q: Hiking is safe on the trail.
+
+r: Berries are ripe along the trail.
+
+Write these propositions using p, q, and r and logical connectives (including negations).
+
+
+(r ∧¬p)
+
+((¬p ∧ q) ∧ r)
+
+r →(q↔¬p)
+
+¬q ∧(¬p ∧r)
+
+q→(¬p ∧¬r)
+
+(p ∧ r) → ¬q */
