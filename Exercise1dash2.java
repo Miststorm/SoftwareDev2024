e61a837a5671dba544ae5f8e16cf7c4df7c168fb
Jared <bishop.jared.michael@gmail.com>
2023-08-21 10:29:40 -0600

    Some name changes and a half-done assignment

---
+++ Exercise1dash2.java
@@ -0,0 +1,35 @@
+public class Exercise1dash2 {
+    public static void main(String[] args) {
+        System.out.println((true && true && true));
+        
+    }
+}
+/*
+1  ((r ∧¬m ∧ ¬b) → g)
+
+
+
+
+
+2. Express these system specifications using the following propositions together with logical connectives (including negations). 
+
+p: “The message is scanned for viruses”
+
+q: “The message was sent from an unknown system”
+
+ (q→p)
+
+(q∧¬p)
+ (q→p) 
+
+(¬q→¬p)
+The following questions relate to inhabitants of the island of knights and knaves created by Smullyan, where knights always tell the truth and knaves always lie. You encounter two people, A and B. Determine, if possible, what A and B are if they address you in the ways described. If you cannot determine what these two people are, can you draw any conclusions?
+A says “At least one of us is a knave” and B says nothing. -- If A is telling the truth, then B is a Knave, and A is a knight. If A is lying, then A is a knave, and B is also a Knave. Either way, we know B must be a Knave.
+
+A says “I am a knave or B is a knight” and B says nothing. -- If A is lying, then he is a Knave. As such, B must also be a Knave. If A is a Knight, then B must be a Knave. Either way, B is a Knave.
+
+A says “We are both knaves” and B says nothing. A must be a Knave, and B must be a Knight.
+
+A detective has interviewed four witnesses to a crime. From the stories of the witnesses the detective has concluded that if the butler is telling the truth then so is the cook; the cook and the gardener cannot both be telling the truth; the gardener and the handyman are not both lying; and if the handyman is telling the truth then the cook is lying. For each of the four witnesses, can the detective determine whether that person is telling the truth or lying? Explain your reasoning. (Hint: This puzzle can be solved by translating statements into logical expressions and reasoning from these expressions using truth tables.)
+Find the output of each of these combinatorial circuits.
+ */
