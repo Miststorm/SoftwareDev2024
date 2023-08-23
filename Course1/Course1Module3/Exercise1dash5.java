4f3d5307924ffdcb1ca050ab5bb4ab89b34126a2
Jared Bishop <bishop.jared.michael@gmail.com>
2023-08-22 22:14:57 -0600

    More Assignments done

--- Course1/Course1Module3/Exercise1dash5.java
+++ Course1/Course1Module3/Exercise1dash5.java
@@ -1,4 +1,6 @@
 package Course1.Course1Module3;
 public class Exercise1dash5 {
-    
+    public static void main(String[] args) {
+        System.out.println(((9.5*4.5) - (2.5*3))/(45.5-3.5));
+    }
 }
