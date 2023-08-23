695cb424314d0314a8ecbf5d4b5b9ab8788d0ede
Jared Bishop <bishop.jared.michael@gmail.com>
2023-08-22 20:40:29 -0600

    Finish Exercise1-14

---
+++ Course1Module3/Exercise1dash14.java
@@ -0,0 +1,22 @@
+package Course1Module3;
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;
+
+public class Exercise1dash14 {
+
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+
+        String name = "Jared Bishop";
+        String date = dtf.format(now);
+        String course = "CSCI 1162 - Development Fundamentals";
+        
+        System.out.println("");
+        System.out.println("Name: " + name);
+        System.out.println("Date: " + date);
+        System.out.println("Course: " + course);
+        System.out.println("");
+        
+    }   
+}
