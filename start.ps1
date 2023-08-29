ecc9fe7be14c8b3281fe0c754e872e7827933c0b
Jared Bishop <97767788+Miststorm@users.noreply.github.com>
2023-08-29 09:13:45 -0600

    Create start.ps1
    file to setup southwest tech computers

---
+++ start.ps1
@@ -0,0 +1,7 @@
+winget install Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
+Set-Location "C:\Users\student\Documents"
+git clone https://github.com/Miststorm/SoftwareDev2024
+git config --global user.name "Jared Bishop"
+git config --global user.email "bishop.jared.michael@gmail.com"
+Set-Location "C:\Users\student\Documents\SoftwareDev"
+code .
