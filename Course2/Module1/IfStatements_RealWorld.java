40ddc9431d5c8a6624a1967bae958c5de109e05f
Jared Bishop <bishop.jared.michael@gmail.com>
2023-09-05 10:18:49 -0600

    Schtuff

---
+++ Course2/Module1/IfStatements_RealWorld.java
@@ -0,0 +1,24 @@
+package Course2.Module1;
+import java.util.Random;
+import java.util.Scanner;
+
+public class IfStatements_RealWorld {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+
+        int coinflip = rand.nextInt(2);
+
+        System.out.print("\nThe computer is flipping a coin. Pick either 0 or 1: ");
+        int userguess = scanner.nextInt();
+
+        if (coinflip == userguess) {
+            System.out.printf("You guessed correctly! You guessed %d and the computer guessed %d", userguess, coinflip);
+        } else {
+            System.out.printf("You were incorrect! You guessed %d and the computer guessed %d", userguess, coinflip);
+        }
+
+        scanner.close();
+    }
+}
