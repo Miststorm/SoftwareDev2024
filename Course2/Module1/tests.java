9f4b943aa9cc7d8bef53d00c308f17490821db2b
Jared Bishop <bishop.jared.michael@gmail.com>
2023-09-11 10:29:01 -0600

    stuff

--- Course2/Module1/tests.java
+++ Course2/Module1/tests.java
@@ -2,16 +2,12 @@ package Course2.Module1;
 import java.util.Scanner;
 public class tests {
     public static void main(String[] args) {
-        boolean var1;
-        boolean var2;
-        for (int i = -1000000000; i < 1000000000; i++) {
-            var1 = ((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0));
-            var2 = ( i % 4 == 0 && i % 100 != 0 || i % 400 == 0);
-        
-            if (var1 != var2) {
-                System.out.println("The two are different when the year is " + i);
-            }
-        }
+        int x = 10;
+        int y = 10;
+
+        System.out.println((y >= 10) && (x-- >= 10));
+        System.out.println(x);
         
     }
 }
+
