3391801f9ab11809288e9d3a072e0e39da067e1d
Jared Bishop <bishop.jared.michael@gmail.com>
2023-09-27 10:32:03 -0600

    asdf

---
+++ Course2/Module3/whileLoopsFundamental.java
@@ -0,0 +1,11 @@
+package Course2.Module3;
+
+public class whileLoopsFundamental {
+    public static void main(String[] args) {
+        int x = 10;
+        while (x >= 1) {
+            System.out.println(x);
+            x -= 1;
+        }
+    }
+}
