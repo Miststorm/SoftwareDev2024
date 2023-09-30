e485eb775af90cfab337ff826db4a3765ba9888c
Jared Bishop <bishop.jared.michael@gmail.com>
2023-09-29 20:15:38 -0600

    5_11 has some very clean code

---
+++ Course2/Module3/module3Tests.java
@@ -0,0 +1,9 @@
+package Course2.Module3;
+import java.lang.Math;
+import java.util.Scanner;
+public class module3Tests {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) { }; System.out.println(i + 4); 
+        }
+    }
